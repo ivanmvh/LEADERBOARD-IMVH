d9a009b542e473dc630f10c77384ae3e13cdeb41
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-12-22 21:28:53 -0500

    Add a little of style

--- src/modules/showData.js
+++ src/modules/showData.js
@@ -1,7 +1,7 @@
 const showData = (data) => {                     
   const scoresContainer = document.querySelector('.scores-container');
   scoresContainer.innerHTML = '';
-  for (let i = 0; i < 10; i += 1) {
+  for (let i = 0; i < 15; i += 1) {
     const newScore = document.createElement('li');
     newScore.innerHTML = `${i + 1}. ${data[i].user}: ${data[i].score}`;
     newScore.classList = 'scores';
