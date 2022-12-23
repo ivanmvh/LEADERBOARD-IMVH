fc77a0efaa61c215771ef50bd960a19e2201098c
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-12-23 01:39:20 -0500

    Add style

--- src/modules/showData.js
+++ src/modules/showData.js
@@ -1,9 +1,21 @@
 const showData = async (data) => {
   const scoresContainer = document.querySelector('.scores-container');
   scoresContainer.innerHTML = '';
+  const titTab = document.createElement('div');
+  titTab.innerHTML = `<div class="tit">
+    <p class="col-index">Pos. </p>
+    <p class="col-name">Leader Name </p>
+    <p class="col-score">Score</p>    
+  </div>`;
+  scoresContainer.appendChild(titTab);
+
   for (let i = 0; i < 15; i += 1) {
     const newScore = document.createElement('li');
-    newScore.innerHTML = `${i + 1}. ${data[i].user}: ${data[i].score}`;
+    newScore.innerHTML = `<li class="score">
+    <p class="col-index">${i + 1} </p>
+    <p class="col-name">${data[i].user} </p>
+    <p class="col-score">${data[i].score}</p>    
+    </li>`;
     newScore.classList = 'scores';
     scoresContainer.appendChild(newScore);
   }
