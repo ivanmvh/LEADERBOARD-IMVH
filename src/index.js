efc6048e7cc35769ab34dbc3abb035d261e1901c
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-12-22 20:53:07 -0500

    Add postNewScore.js module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import getAllScoresData from './modules/getServerData.js'; 
+import postNewScore from './modules/postNewScore';
 import './style.css';
 
-
+// refresh button
 const refreshScoresBtn = document.querySelector('.refresh-scores');
+
+// Form fields
+const leaderName = document.querySelector('.leader-name');
+const leaderScore = document.querySelector('.leader-score');
+const leaderForm = document.querySelector('.leader-form');
+
+// form submit button event
+leaderForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  await postNewScore(leaderName.value, parseInt(leaderScore.value, 10));
+  leaderName.value = '';
+  leaderScore.value = '';
+});
 
 refreshScoresBtn.addEventListener('click', () => {
   getAllScoresData();
