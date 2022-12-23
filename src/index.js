c92aab0709b58ae9ac269dc04eaf652e3c460e0e
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-12-22 20:35:37 -0500

    Add refresh scores event

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import getAllScoresData from './modules/getServerData.js'; 
 import './style.css';
 
+
+const refreshScoresBtn = document.querySelector('.refresh-scores');
+
+refreshScoresBtn.addEventListener('click', () => {
+  getAllScoresData();
+});
+
 getAllScoresData();
 
 
